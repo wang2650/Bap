9ee3d18f9761d53bcc0c26f37ffe83219e242aac
wang2650 <wang2650@163.com>
2019-07-23 23:38:42 +0800

    字典部分

--- WebApi/Controllers/configmanage/DictController.cs
+++ WebApi/Controllers/configmanage/DictController.cs
@@ -28,10 +28,10 @@ namespace WebApi.Controllers.configmanager
         /// <param name="model"></param>
         /// <returns></returns>
         [HttpPost]
-        [AllowAnonymous]
         [WebApi.Common.Log]
         [Route("InsertDict")]
-        public JsonResult InsertDict([FromForm]  WXQ.InOutPutEntites.Input.configmanager.Dict.InsertDictInput model)
+        [Authorize("common")]
+        public JsonResult InsertDict([FromBody]  WXQ.InOutPutEntites.Input.configmanager.Dict.InsertDictInput model)
         {
             ResponseResult result = new ResponseResult();
 
@@ -81,10 +81,10 @@ namespace WebApi.Controllers.configmanager
         /// <param name="model"></param>
         /// <returns></returns>
         [HttpPost]
-        [AllowAnonymous]
         [WebApi.Common.Log]
         [Route("UpdateDict")]
-        public JsonResult UpdateDict([FromForm]  WXQ.InOutPutEntites.Input.configmanager.Dict.UpdateDictInput model)
+        [Authorize("common")]
+        public JsonResult UpdateDict([FromBody]  WXQ.InOutPutEntites.Input.configmanager.Dict.UpdateDictInput model)
         {
             ResponseResult result = new ResponseResult();
 
@@ -133,11 +133,11 @@ namespace WebApi.Controllers.configmanager
         /// </summary>
         /// <param name="Id"></param>
         /// <returns></returns>
-        [HttpPost]
-        [AllowAnonymous]
+        [HttpGet]
         [WebApi.Common.Log]
         [Route("DeleteDictByDictId")]
-        public JsonResult DeleteDictByDictId([FromForm]  int Id)
+        [Authorize("common")]
+        public JsonResult DeleteDictByDictId([FromQuery]  int Id)
         {
             ResponseResult result = new ResponseResult();
 
@@ -171,10 +171,10 @@ namespace WebApi.Controllers.configmanager
         /// <param name="GroupName"></param>
         /// <returns></returns>
         [HttpPost]
-        [AllowAnonymous]
         [WebApi.Common.Log]
         [Route("DeleteDictByGroupName")]
-        public JsonResult DeleteDictByGroupName([FromForm]  string GroupName)
+        [Authorize("common")]
+        public JsonResult DeleteDictByGroupName([FromQuery]  string GroupName)
         {
             ResponseResult result = new ResponseResult();
 
@@ -213,7 +213,7 @@ namespace WebApi.Controllers.configmanager
         [AllowAnonymous]
         [WebApi.Common.Log]
         [Route("GetDictList")]
-        public JsonResult GetDictList([FromForm]  WXQ.InOutPutEntites.Input.configmanager.Dict.GetDictListInput model)
+        public JsonResult GetDictList([FromBody]  WXQ.InOutPutEntites.Input.configmanager.Dict.GetDictListInput model)
         {
             ResponseResult result = new ResponseResult();
 
@@ -247,7 +247,47 @@ namespace WebApi.Controllers.configmanager
         }
 
 
+        /// <summary>
+        /// 搜索列表
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [AllowAnonymous]
+        [WebApi.Common.Log]
+        [Route("GetGroupList")]
+        public JsonResult GetGroupList([FromBody]  WXQ.InOutPutEntites.Input.configmanager.Dict.GetDictListInput model)
+        {
+            ResponseResult result = new ResponseResult();
 
+            GetDictListInputModelValidation validator = new GetDictListInputModelValidation();
+            ValidationResult vr = validator.Validate(model);
+
+            if (!vr.IsValid)
+            {
+                result.Code = ResponseResultMessageDefine.ParaError;
+                result.Errors = vr.Errors.Select(e => e.ErrorMessage).ToList();
+            }
+            else
+            {
+                WXQ.BusinessCore.CommonManager.DictOp op = new WXQ.BusinessCore.CommonManager.DictOp();
+
+                SqlSugar.PageModel pagemodel = TypeAdapter.Adapt<WXQ.InOutPutEntites.Input.PageInput, SqlSugar.PageModel>(model.Page);
+                ListResult<WXQ.Enties.Dict> rv = op.GetDictList("dictgroup", pagemodel);
+
+                if (rv == null)
+                {
+                    result.Code = ResponseResultMessageDefine.OpLost;
+                    result.Errors.Add(ResponseResultMessageDefine.OpLostMessage);
+                }
+                else
+                {
+                    result.Data = rv;
+                }
+            }
+
+            return Json(result);
+        }
 
 
 
