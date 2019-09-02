ffaef5fbc0ca4aaa00bb8c19546aa2a547913a65
wang2650 <wang2650@163.com>
2019-09-02 09:45:47 +0800

    提交

---
+++ Common/YouMeng/Enums/EquipmentTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.YouMeng.Enums
+{
+    public enum EquipmentTypeEnum
+    {
+        Android=1,
+        IOS=2
+    }
+}
