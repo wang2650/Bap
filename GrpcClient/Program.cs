ebf2535875613869fa79e4a2725d5b8cd85a6019
wang2650 <wang2650@163.com>
2019-09-19 09:33:53 +0800

    添加grpc的项目

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using Grpc.Core;
+using MagicOnion.Client;
+using System;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        private static Channel _channel;
+        private static IMyFirstService _client;
+
+        static void Main(string[] args)
+        {
+            _channel = new Channel("localhost", 12345, ChannelCredentials.Insecure);
+
+            // get MagicOnion dynamic client proxy
+            _client = MagicOnionClient.Create<IMyFirstService>(_channel);
+            testy();
+        }
+
+        public async static void testy()
+        {
+        
+
+            // call method.
+            var result = _client.SumAsync(100, 200).ResponseAsync.Result;
+            Console.WriteLine("客户端结果:" + result);
+
+            result = _client.SumAsync(190, 200).ResponseAsync.Result;
+            Console.WriteLine("客户端结果:" + result);
+
+
+            result = _client.SumAsync(10, 290).ResponseAsync.Result;
+            Console.WriteLine("客户端结果:" + result);
+
+
+            result = _client.SumAsync(65, 233).ResponseAsync.Result;
+            Console.WriteLine("客户端结果:" + result);
+
+        }
+
+
+
+    }
+}
---
+++ GrpcServer/MyFirstService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Grpc.Core;
+using MagicOnion;
+using MagicOnion.Server;
+using System;
+namespace GrpcServer
+{
+    public class MyFirstService : ServiceBase<IMyFirstService>, IMyFirstService
+    {
+        // You can use async syntax directly.
+        public async UnaryResult<int> SumAsync(int x, int y)
+        {
+            Logger.Debug($"Received:{x}, {y}");
+
+            return x + y;
+        }
+    }
+}
