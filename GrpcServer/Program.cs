4234b0200c5aadc494a1653562a61ec103fd85dd
wang2650 <wang2650@163.com>
2019-09-26 14:09:01 +0800

    添加newlift apiserver测试项目

--- GrpcServer/Program.cs
+++ GrpcServer/Program.cs
@@ -1,14 +1,33 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Grpc.Core;
 using MagicOnion;
 using MagicOnion.Server;
+using MagicOnion.Hosting;
+using Microsoft.Extensions.Hosting;
 namespace GrpcServer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             GrpcEnvironment.SetLogger(new Grpc.Core.Logging.ConsoleLogger());
+
+
+            //// you can use new HostBuilder() instead of CreateDefaultBuilder
+            await new HostBuilder()
+                .UseMagicOnion()
+                .RunConsoleAsync();
+
+
+            //MagicOnionServiceDefinition service = MagicOnionEngine.BuildServerServiceDefinition(
+
+            //    new[] { typeof(IMyFirstService).Assembly },  // 加载引用程序集
+            //    new MagicOnionOptions(true)
+            //    {
+            //        MagicOnionLogger = new MagicOnionLogToGrpcLogger()
+            //    }
+            //);
 
             // setup MagicOnion and option.
             var service = MagicOnionEngine.BuildServerServiceDefinition(isReturnExceptionStackTraceInErrorDetail: true);
---
+++ newlifeapiclient/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System;
+using System.Diagnostics;
+using NewLife.Data;
+using NewLife.Log;
+using NewLife.Net;
+using NewLife.Net.Handlers;
+using NewLife.Remoting;
+using NewLife.Threading;
+namespace newlifeapiclient
+{
+    class Program
+    {
+        static TimerX _timer;
+        static ApiServer _server;
+        static void Main(string[] args)
+        {
+            XTrace.UseConsole();
+
+            try
+            {
+               
+                    TestClient();
+            }
+            catch (Exception ex)
+            {
+                XTrace.WriteException(ex);
+            }
+
+            Console.WriteLine("OK!");
+            Console.ReadKey();
+        }
+
+        static async void TestClient()
+        {
+            var client = new MyClient("tcp://127.0.0.1:8084");
+
+            // 指定编码器
+            client.Encoder = new JsonEncoder();
+           // client.EncoderLog = XTrace.Log;
+
+            //// 打开原始数据日志
+            //var ns = client.Client;
+            //ns.Log = XTrace.Log;
+            //ns.LogSend = true;
+            //ns.LogReceive = true;
+
+            // 定时显示性能数据
+            client.StatPeriod = 5;
+
+           // client.Log = XTrace.Log;
+            client.Open();
+
+            // 定时显示性能数据
+            //_timer = new TimerX(ShowStat, ns, 100, 1000);
+
+            // 标准服务，Json
+            var n = await client.AddAsync(1245, 3456);
+    
+
+            // 高速服务，二进制
+            var buf = "Hello".GetBytes();
+            var pk = await client.RC4Async(buf);
+          
+
+            // 返回对象
+            var user = await client.FindUserAsync(123, true);
+       
+            // 拦截异常
+            try
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Reset();
+                sw.Start();
+                int res = 0;
+                for (int i=0;i<10000;i++)
+                {
+                     res = await client.AddAsync(123, i);
+                
+                }
+                sw.Stop();
+                Console.WriteLine("用时"+ sw.ElapsedMilliseconds.ToString());
+              
+                
+            }
+
+            catch (ApiException ex)
+            {
+               // XTrace.WriteLine("FindUser出错，错误码={0}，内容={1}", ex.Code, ex.Message);
+            }
+        }
+    }
+}
