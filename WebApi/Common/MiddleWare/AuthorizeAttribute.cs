98541bd7e8dd1fd26c1bfc52a99b04bb6ed4ff72
wang2650 <wang2650@163.com>
2019-10-28 21:24:18 +0800

    netcore3

--- WebApi/Common/MiddleWare/AuthorizeAttribute.cs
+++ WebApi/Common/MiddleWare/AuthorizeAttribute.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -16,31 +16,36 @@ namespace WebApi.Common.MiddleWare
         public void OnAuthorization(AuthorizationFilterContext context)
         {
         }
-
     }
+
     /// <summary>
     /// 验证
     /// </summary>
 
-        public class AuthorizeAttribute : Attribute, IAuthorizationFilter
+    public class AuthorizeAttribute : Attribute, IAuthorizationFilter, IAsyncAuthorizationFilter
     {
-
-
         public string AccessKey { get; set; }
 
+        public  Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+
+            context.Result = new UnauthorizedResult();
+          //  context.HttpContext.Response.WriteAsync("Executing AuthorizationFilter. \r\n");
+
+            return Task.CompletedTask;
+        }
 
         public virtual void OnAuthorization(AuthorizationFilterContext filterContext)
         {
             if (filterContext == null)
                 throw new ArgumentNullException(nameof(filterContext));
-          
-            //filterContext.Result = new Microsoft.AspNetCore.Mvc.JsonResult(new { name = "kxy1" });
+
+            filterContext.Result = new Microsoft.AspNetCore.Mvc.JsonResult(new { name = "kxy1" });
 
             var identity = new CustomIdentity("10");
 
             var principal = new ClaimsPrincipal(identity);
             filterContext.HttpContext.User = principal;
         }
-
     }
 }
