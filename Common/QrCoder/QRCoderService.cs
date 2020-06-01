c7c6170b5af716b49f41ae6270298060733fb896
wang2650 <wang2650@163.com>
2020-06-01 11:01:09 +0800

    添加读取二维码的方法

--- Common/QrCoder/QRCoderService.cs
+++ Common/QrCoder/QRCoderService.cs
@@ -1,8 +1,13 @@
 ﻿
 
+using Common.QrCoder;
 using CommonLib.Extensions.Common;
 using CommonLib.QrCoder.QrCode;
 using QRCoder;
+using System;
+using System.Drawing;
+using ThoughtWorks.QRCode.Codec;
+using ThoughtWorks.QRCode.Codec.Data;
 
 namespace CommonLib.QrCoder.QrCoder {
     /// <summary>
@@ -75,5 +80,169 @@ namespace CommonLib.QrCoder.QrCoder {
             BitmapByteQRCode qrCode = new BitmapByteQRCode( data );
             return qrCode.GetGraphic( _size );
         }
-    }
+
+        #region  使用 ThoughtWorks.QRCode.Codec
+
+        /// <summary>
+        /// 定义参数,生成二维码
+        /// </summary>
+        public static QrResult Create(string text, string path)
+        {
+            QrResult result = new QrResult();
+            try
+            {
+                if (string.IsNullOrEmpty(text) ||string.IsNullOrEmpty(path))
+                {
+                    result.IsSuccess = false;
+                    result.ReturnMessage = "二维码内容和二维码图片路径不可为空";
+                }
+                else
+                {
+                    var image = Encode(text);
+                    if (image == null)
+                    {
+                        result.IsSuccess = false;
+                        result.ReturnMessage = "图片生成失败";
+                    }
+                    else
+                    {
+                        image.Save(path);
+                    }
+                }
+
+          
+              
+            }
+            catch (Exception ex)
+            {
+                result.IsSuccess = false;
+                result.ReturnMessage = ex.Message;
+            }
+            return result;
+        }
+
+
+
+
+
+
+        /// <summary>
+        /// 返回二维码图片
+        /// </summary>
+        public static Bitmap Encode(string text)
+        {
+            try
+            {
+                var qrCodeEncoder = new QRCodeEncoder();
+                qrCodeEncoder.QRCodeVersion = 5;
+                qrCodeEncoder.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE;
+                qrCodeEncoder.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.M;
+                qrCodeEncoder.QRCodeScale = 4;
+                return qrCodeEncoder.Encode(text);
+            }
+            catch (Exception ex)
+            {
+             
+                return null;
+            }
+        }
+
+
+
+
+
+
+
+
+        /// <summary>
+        /// 获取二维码内容
+        /// </summary>
+        /// <param name="image"></param>
+        /// <returns></returns>
+        public static QrResult Decode(Bitmap image)
+        {
+            QrResult result = new QrResult();
+            try
+            {
+          
+                var qrCodeBitmapImage = new QRCodeBitmapImage(image);
+                var qrCodeDecoder = new QRCodeDecoder();
+                result.ReturnMessage= qrCodeDecoder.decode(qrCodeBitmapImage); 
+            }
+            catch (Exception ex)
+            {
+
+                result.IsSuccess = false;
+                result.ReturnMessage = ex.Message;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 解析图片的的内容
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public  static QrResult Decode(string filePath)
+        {
+            QrResult result = new QrResult();
+            try
+            {
+
+                if (string.IsNullOrEmpty(filePath))
+                {
+                    result.IsSuccess = false;
+                    result.ReturnMessage = "路径为空";
+                }
+                else
+                {
+                    try{
+                        var pic = new Bitmap(filePath);
+                        if (pic!=null)
+                        {
+                            result= Decode(new Bitmap(filePath));
+                        }
+                        else
+                        {
+                            result.IsSuccess = false;
+                            result.ReturnMessage = "图片不存在或者非图片格式";
+                           
+
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        result.IsSuccess = false;
+                        result.ReturnMessage = ex.Message;
+                        return result;
+                    }
+                 
+                  
+
+
+                }
+                 
+            }
+            catch(Exception ex)
+            { 
+                
+              
+                result.IsSuccess = false;
+                result.ReturnMessage = ex.Message;
+                return result;
+
+            }
+            return result;
+
+
+
+        }
+
+            #endregion
+
+
+
+
+
+        }
 }
---
+++ Common/QrCoder/QrResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.QrCoder
+{
+    public class QrResult
+    {
+
+        public bool IsSuccess { get; set; } = true;
+
+
+        public string ReturnMessage { get; set; } = string.Empty;
+
+        public byte[] ReturnData { get; set; }
+
+
+
+    }
+}
